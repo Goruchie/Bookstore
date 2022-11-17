7164b82dc5b5b48cc209d86960b23408b2798ea9
Marcos Gualtero Lourenzo <marcosgualtero43253@gmail.com>
2022-11-17 14:31:50 -0300

    fix import issue

--- src/redux/congigureStore.js
+++ src/redux/congigureStore.js
@@ -1,6 +1,6 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import initialState from './books/boooks-redux';
-import checkStatus from './categories/categories-redux';
+import initialState from './books/books';
+import checkStatus from './categories/categories';
 
 // function that create a combine reducers with the function about book and categories
 const rootReducer = combineReducers({
